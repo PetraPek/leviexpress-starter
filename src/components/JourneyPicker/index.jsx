de4c24a28fac6e4714069d109aa9aea66f735163
Petra Pekařová <petra.pekarova@seznam.cz>
2023-05-27 14:59:53 +0200

    Přidání vyhledání spoje

--- src/components/JourneyPicker/index.jsx
+++ src/components/JourneyPicker/index.jsx
@@ -1,12 +1,52 @@
 import React, { useEffect, useState } from 'react';
 import './style.css';
 
+const CityOptions = ({ cities }) => {
+  return (
+    <>
+      <option key="blank" value="">
+        Vyberte
+      </option>
+      {cities.map((city) => (
+        <option key={city.code} value={city.code}>
+          {city.name}
+        </option>
+      ))}
+    </>
+  );
+};
+
+const DateOptions = ({ dates }) => {
+  return (
+    <>
+      <option key="blank" value="">
+        Vyberte
+      </option>
+      {dates.map((date) => (
+        <option key={date.dateBasic} value={date.dateBasic}>
+          {date.dateCs}
+        </option>
+      ))}
+    </>
+  );
+};
+
 export const JourneyPicker = ({ onJourneyChange }) => {
+  const [cities, setCities] = useState([]);
+  const [dates, setDates] = useState([]);
   const [fromCity, setFromCity] = useState('');
   const [toCity, setToCity] = useState('');
   const [date, setDate] = useState('');
-  const [cities, setCities] = useState([]);
-  const [dates, setDates] = useState([]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    fetch(
+      `https://apps.kodim.cz/daweb/leviexpress/api/journey?fromCity=${fromCity}&toCity=${toCity}&date=${date}`,
+    )
+      .then((response) => response.json())
+      .then((data) => onJourneyChange(data.results));
+  };
 
   useEffect(() => {
     fetch('https://apps.kodim.cz/daweb/leviexpress/api/cities')
@@ -15,20 +55,25 @@ export const JourneyPicker = ({ onJourneyChange }) => {
   }, []);
 
   useEffect(() => {
-    fetch('https://apps.kodim.cz/daweb/leviexpress/api/dates')
-      .then((response) => response.json())
-      .then((dates) => setDates(dates.results));
+    fetch('https://apps.kodim.cz/daweb/leviexpress/api/dates').then(
+      (response) => response.json().then((data) => setDates(data.results)),
+    );
   }, []);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  };
+  let submitDisabled = false;
+  if (fromCity === '' || toCity === '' || date === '') {
+    submitDisabled = true;
+  }
+
+  if (fromCity === toCity) {
+    submitDisabled = true;
+  }
 
   return (
     <div className="journey-picker container">
       <h2 className="journey-picker__head">Kam chcete jet?</h2>
       <div className="journey-picker__body">
-        <form onSubmit={handleSubmit} className="journey-picker__form">
+        <form className="journey-picker__form" onSubmit={handleSubmit}>
           <label>
             <div className="journey-picker__label">Odkud:</div>
             <select
@@ -47,11 +92,11 @@ export const JourneyPicker = ({ onJourneyChange }) => {
           <label>
             <div className="journey-picker__label">Datum:</div>
             <select value={date} onChange={(e) => setDate(e.target.value)}>
-              <DatesOptions dates={dates} />
+              <DateOptions dates={dates} />
             </select>
           </label>
           <div className="journey-picker__controls">
-            <button className="btn" type="submit">
+            <button className="btn" type="submit" disabled={submitDisabled}>
               Vyhledat spoj
             </button>
           </div>
@@ -59,31 +104,5 @@ export const JourneyPicker = ({ onJourneyChange }) => {
         <img className="journey-picker__map" src="/map.svg" />
       </div>
     </div>
-  );
-};
-
-const CityOptions = ({ cities }) => {
-  return (
-    <>
-      <option value="">Vyberte</option>
-      {cities.map((city) => (
-        <option key={city.code} value={city.code}>
-          {city.name}
-        </option>
-      ))}
-    </>
-  );
-};
-
-const DatesOptions = ({ dates }) => {
-  return (
-    <>
-      <option value="">Vyberte</option>
-      {dates.map((date) => (
-        <option key={date.dateBasic} value={date.dateBasic}>
-          {date.dateCs}
-        </option>
-      ))}
-    </>
   );
 };
