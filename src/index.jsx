d51dfd10ef74e547621c66a428da73581aa7ac41
Petra Pekařová <petra.pekarova@seznam.cz>
2023-05-27 10:57:18 +0200

    Routování

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './components/App';
+
 import './style.css';
 
 export const API_BASE_URL = 'https://apps.kodim.cz/daweb/leviexpress/api';
 
-createRoot(document.querySelector('#app')).render(<App />, );
+createRoot(document.querySelector('#app')).render(<App />);
